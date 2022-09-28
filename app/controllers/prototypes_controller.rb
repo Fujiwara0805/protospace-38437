85f36c56e64e5d601cdf0496e0e8f3c31dde578a
Fujiwara0805 <tiki4091@gmail.com>
2022-09-28 18:02:39 +0900

    second commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,8 +1,7 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:index, :show]
-  before_action :set_tweet, only: [:edit, :show]
   before_action :move_to_index, except: [:index, :show] 
-
+  before_action :set_tweet, only: [:edit, :show]
+  
   def index
     @prototypes = Prototype.all
   end
